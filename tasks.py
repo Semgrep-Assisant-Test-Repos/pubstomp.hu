6f9b360d28134c6c2735482cffdc13e3d2f78c89
Bence Nagy <underer@gmail.com>
2014-05-03 19:20:17 +0200

    Empty Flask project

---
+++ tasks.py
@@ -0,0 +1,40 @@
+import os
+
+from invoke import task, run
+from csscompressor import compress as css_minify
+from slimit import minify as js_minify
+
+
+@task
+def build():
+    for filename in os.listdir('app/assets/css'):
+        if filename.endswith('.sass') and not filename.startswith('_'):
+            sass_file_path = os.path.join('duchess/assets/css/', filename)
+            css_file_path = sass_file_path + '.css'
+
+            run('isass %s --output %s' % (sass_file_path, css_file_path))
+
+    for filename in os.listdir('app/assets/css'):
+        if filename.endswith('.sass.css'):
+            css_file_path = os.path.join('duchess/assets/css/', filename)
+            min_css_file_path = css_file_path.replace('.sass.css', '.min.css')
+            with open(css_file_path) as css_file:
+                with open(min_css_file_path, 'w') as min_css_file:
+                    min_css_file.write(css_minify(css_file.read()))
+            os.remove(css_file_path)
+
+    for filename in os.listdir('app/assets/js'):
+        if filename.endswith('.js') and '.min.js' not in filename:
+            js_file_path = os.path.join('app/assets/js/', filename)
+            min_js_file_path = js_file_path.replace('.js', '.min.js')
+            js_file = open(js_file_path)
+            min_js_file = open(min_js_file_path, 'w')
+
+            min_js_file.write(
+                js_minify(js_file.read(), mangle=True, mangle_toplevel=True)
+            )
+
+
+@task('build')
+def start():
+    run('./run.py')
