71daefc86e4ababcd6591f4c97e11e830ce5ff21
Bence Nagy <underer@gmail.com>
2014-05-21 02:12:21 +0200

    Underyxify design, and some other fixes, too

--- tasks.py
+++ tasks.py
@@ -1,28 +1,11 @@
 import os
 
 from invoke import task, run
-from csscompressor import compress as css_minify
 from slimit import minify as js_minify
 
 
 @task
 def build():
-    # Piece of ass SASS compiler
-
-    #for filename in os.listdir('app/assets/css'):
-    #    if filename.endswith('.sass') and not filename.startswith('_'):
-    #        sass_file_path = os.path.join('app/assets/css/', filename)
-    #        css_file_path = sass_file_path + '.css'
-    #
-    #        run('isass %s --output %s' % (sass_file_path, css_file_path))
-    #
-    #for filename in os.listdir('app/assets/css'):
-    #    if filename.endswith('.sass.css'):
-    #        css_file_path = os.path.join('app/assets/css/', filename)
-    #        min_css_file_path = css_file_path.replace('.sass.css', '.min.css')
-    #        with open(css_file_path) as css_file:
-    #            with open(min_css_file_path, 'w') as min_css_file:
-    #                min_css_file.write(css_minify(css_file.read()))
 
     for filename in os.listdir('app/assets/js'):
         if filename.endswith('.js') and '.min.js' not in filename:
