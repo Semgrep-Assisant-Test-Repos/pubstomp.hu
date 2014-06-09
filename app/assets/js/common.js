8fa056884fb107a885d84261977a2b58bd8a8d63
Bence Nagy <underer@gmail.com>
2014-06-10 01:33:14 +0200

    Google Analytics event tracking

--- app/assets/js/common.js
+++ app/assets/js/common.js
@@ -6,8 +6,10 @@ $(function(){
             data: {'value': $(this).attr('value')}
         });
         $('.confirmation-message').addClass($(this).attr('value'))
+        _gaq.push(['_trackEvent', 'vote', $(this).attr('value')]);
     });
     $('.poll .change').click(function(){
         $('.confirmation-message').removeClass('no maybe yes')
+        _gaq.push(['_trackEvent', 'change']);
     });
 });
